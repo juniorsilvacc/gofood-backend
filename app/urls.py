b7e3d0003dc6cfecafa0d17f5830f5932b7f1acc
juniorsilvacc <juniorsilvacc@hotmail.com>
2024-11-01 18:34:47 -0300

    upload of images products

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -12,4 +14,4 @@ urlpatterns = [
     # path('api/v1/', include('addresses.urls')),
     # path('api/v1/', include('orders.urls')),
     # path('api/v1/', include('items.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from products.models import Product, Option, Additional
+from django.utils.html import format_html
 
 
 @admin.register(Option)
@@ -16,6 +17,12 @@ class AdditionalAdmin(admin.ModelAdmin):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category', 'price', 'active',)
+    list_display = ('name', 'display_image', 'category', 'price', 'active',)
     search_fields = ('name',)
     list_filter = ('category',)
+
+    def display_image(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" style="width: 50px; height: auto;" />', obj.image.url)
+        return "No Image"
+    display_image.short_description = 'Image'
